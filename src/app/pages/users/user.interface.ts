1e1bf60cd55b39fda52597156be317421c69614a
bezael <bezael@gmail.com>
2023-02-13 17:04:49 +0100

    feat add countryName property

--- src/app/pages/users/user.interface.ts
+++ src/app/pages/users/user.interface.ts
@@ -11,4 +11,5 @@ export interface User {
   phone: string;
   gender: string;
   location: Location;
+  countryName?: string;
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,8 +1,8 @@
 import { UsersService } from './users.service';
 import { Component, inject } from '@angular/core';
-import { tap } from 'rxjs';
+import { tap, map } from 'rxjs';
 import { User } from './user.interface';
-
+import { countryCodes } from '@shared/mock_data/country-codes';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -17,6 +17,12 @@ export class UsersComponent {
     this.usersSvc.getUsers$()
       .pipe(
         tap((users: User[]) => console.log(users)),
+        map(users => {
+         return users.map((user: User) => ({
+            ...user,
+            countryName: countryCodes[user.location.country] || "Unknown"
+          }));
+        }),
         tap((users:User[]) => this.users = users)
       )
       .subscribe()
