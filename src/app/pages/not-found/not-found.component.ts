9ac469fdc4f07582d796ca27f0f5f4563a7dc676
bezael <bezael@gmail.com>
2023-02-13 19:02:21 +0100

    fix not found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule],
 })
-export class NotFoundComponent  {
-
-
-}
+export class NotFoundComponent {}
